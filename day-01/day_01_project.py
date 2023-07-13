ccb2629cb7b88a525f2dfa068bf97923888ed6b6
omar-talat <dr.omartalat@gmail.com>
2023-07-13 23:48:46 +0300

    day_01 codes added

---
+++ day-01/day_01_project.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print(" hi!")
+#2. Ask the user for the city that they grew up in.
+birthPlace = input("Where are you from?\n")
+#3. Ask the user for the name of a pet.
+name = input("What is your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+brandName = birthPlace + " " + name
+#5. Make sure the input cursor shows on a new line:
+print(brandName) 
---
+++ day-01/exercise_04.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+x = a
+a = b
+b = x
+print("a: " + a)
+print("b: " + b)
