79f1350c261db1325d63f9834f4637c296c424d4
Omar <dr.omartalat@gmail.com>
2024-05-26 10:39:44 +0300

    day 024 updated

--- 024-day/mail_merge_project/main.py
+++ 024-day/mail_merge_project/main.py
@@ -9,20 +9,21 @@ Hint1: This method will help you: https://www.w3schools.com/python/ref_file_read
 Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 """
+PLACEHOLDER = "[name]"
 
 
 # step 1: create list of names
-invited_names = []
-with open("./input/names/invited_names.txt") as names:
-    for name in names:
-        invited_names.append(name.strip())
-    print(invited_names)
+with open("./input/names/invited_names.txt") as f:
+    names = f.readlines()
 
 # step 3: create different files for each name
 
-for i in range(len(invited_names)):
+for name in names:
 
     # step 2: Replace the [name] placeholder with the actual name
 
     with open("./input/letters/starting_letter.txt", mode="r+") as f:
-        f.readline().replace("[name]", invited_names[i])
+        old_letter = f.read()
+        new_letter = old_letter.replace(PLACEHOLDER, name.strip())
+        with open(f"./output/readytosend/{name.strip()}.txt", mode="w") as file:
+            file.write(new_letter)
